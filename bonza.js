50539a40d531556a24ce23d21e272f9801292efa
rmovchan <roman.movchan@gmail.com>
2015-02-13 11:05:59 +1100

    New directory structure

--- bonza.js
+++ bonza.js
@@ -1,7 +1,7 @@
 /**
  * @author Roman Movchan
  */
-function BonzaInit() {
+function Bonza() {
 	function evalFormula(formula, context, output) {
 
 		var scanner = /\s*(-?\d*\.\d+)|(-?\d+)|(\w+)|(\".*\")|('.*')|(\+)|(-)|(\*)|(\/)|(\.)|(\()|(\))|(\[)|(\])|(\{)|(\})|(:)|(,)|(<=?)|(\/?=)|(>=?)/g;
@@ -289,8 +289,8 @@ function BonzaInit() {
 
 		try {
 			if (parseFormula()) {
-				if (result === false) { //Bonza has no booleans
-					return false;
+				if (result === true || result === false) {//Bonza has no booleans
+					return result;
 				}
 				output.result = result;
 				return true;
@@ -302,196 +302,6 @@ function BonzaInit() {
 		}
 	};
 
-	function evalExpr(expr, context, output) {
-		var stmt;
-		var where;
-		var i;
-		var result = { };
-		var context2 = { };
-		var output2 = { };
-		var prop;
-		var array = [];
-		var parser;
-		var xmlDoc;
-		var temp;
-		var first;
-		try {
-			if (expr.nodeType == 3) {
-				return evalFormula(expr.nodeValue.trim(), context, output);
-			}
-			switch(expr.nodeName) {
-			case "text":
-				output.result = expr.innerHTML;
-				break;
-			case "eval":
-				first = 0;
-				{
-				}
-				while (expr.childNodes[first].nodeType == 3 && expr.childNodes[first].nodeValue.trim() == "")
-				first++;
-				if (evalExpr(expr.childNodes[first], context, result)) {
-					if (window.DOMParser) {
-						parser = new DOMParser();
-						xmlDoc = parser.parseFromString(result.result, "text/xml");
-					} else {
-						xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
-						xmlDoc.async = false;
-						xmlDoc.loadXML(result.result);
-					}
-					return evalExpr(xmlDoc.children[0], context, output);
-				} else {
-					output.result = undefined;
-					return false;
-				}
-				break;
-			case "list":
-				temp = expr.getElementsByTagName("item");
-				array.length = temp.length;
-				for ( i = 0; i < array.length; i++) {
-					if (evalExpr(temp[i].childNodes[0], context, result)) {
-						array[i] = result.result;
-					} else {
-						output.result = undefined;
-						return false;
-					}
-				}
-				output.result = array;
-				break;
-			case "calc":
-				first = 0;
-				while (expr.childNodes[first].nodeType == 3 && expr.childNodes[first].nodeValue.trim() == "")
-				first++;
-				where = expr.getElementsByTagName("where");
-				for (prop in context) {
-					context2[prop] = context[prop];
-				}
-				for ( i = where.length - 1; i >= 0; i--) {
-					stmt = where[i].children[0];
-					if (evalStmt(stmt, context2, result)) {
-						for (prop in result) {
-							context2[prop] = result[prop];
-						}
-					} else {
-						output.result = undefined;
-						return false;
-					}
-				}
-				return evalExpr(expr.childNodes[first], context2, output);
-			case "wrap":
-				for (prop in context) {
-					context2[prop] = context[prop];
-				}
-				for ( i = 0; i < stmt.childNodes.length; i++) {
-					if (stmt.childNodes[i].nodeType != 3) {
-						if (evalStmt(stmt.childNodes[i], context2, result)) {
-							for (prop in result) {
-								context2[prop] = result[prop];
-								output2[prop] = result[prop];
-							}
-							result = { };
-						} else {
-							return false;
-						}
-					}
-				}
-				output.result = output2;
-				break;
-			default:
-				return false;
-			}
-			return true;
-		} catch(error) {
-			return false;
-		}
-	};
-
-	function evalStmt(stmt, context, output) {
-		var name;
-		var i;
-		var prop;
-		var context2 = { };
-		var stmt2;
-		var result = { };
-		var first;
-		try {
-			switch(stmt.nodeName) {
-			case "is":
-				where = expr.getElementsByTagName("where");
-				for (prop in context) {
-					context2[prop] = context[prop];
-				}
-				for ( i = where.length - 1; i >= 0; i--) {
-					stmt2 = where[i].children[0];
-					if (evalStmt(stmt2, context2, result)) {
-						for (prop in result) {
-							context2[prop] = result[prop];
-						}
-					} else {
-						return false;
-					}
-				}
-				first = 0;
-				while (expr.childNodes[first].nodeType == 3 && expr.childNodes[first].nodeValue.trim() == "")
-				first++;
-				return evalFormula(stmt.childNodes[first].nodeValue.trim(), context2, result);
-			case "not":
-				return !evalStmt(stmt.children[0], context, result);
-			case "def":
-				name = stmt.getAttribute("var");
-				first = 0;
-				while (stmt.childNodes[first].nodeType == 3 && stmt.childNodes[first].nodeValue.trim() == "")
-				first++;
-				if (evalExpr(stmt.childNodes[first], context, result)) {
-					output[name] = result.result;
-				} else {
-					return false;
-				}
-				break;
-			case "all":
-				for (prop in context) {
-					context2[prop] = context[prop];
-				}
-				for ( i = 0; i < stmt.children.length; i++) {
-					if (evalStmt(stmt.children[i], context2, result)) {
-						for (prop in result) {
-							context2[prop] = result[prop];
-							output[prop] = result[prop];
-						}
-						result = { };
-					} else {
-						output = { };
-						return false;
-					}
-				}
-				break;
-			case "any":
-				for ( i = 0; i < stmt.children.length; i++) {
-					if (evalStmt(stmt.children[i], context, result)) {
-						for (prop in result) {
-							output[prop] = result[prop];
-						}
-						return true;
-					}
-				}
-				return false;
-			case "unwrap":
-				first = 0;
-				while (expr.childNodes[first].nodeType == 3 && expr.childNodes[first].nodeValue.trim() == "")
-				first++;
-				if (evalExpr(stmt.childNodes[first], context, result)) {
-					for (prop in result.result) {
-						output[prop] = result.result[prop];
-					}
-				}
-				break;
-			default:
-				return false;
-			}
-			return true;
-		} catch(error) {
-			return false;
-		}
-	};
 	var core = {
 		classname : function(name) {
 			return "bonza-" + name;
@@ -518,6 +328,379 @@ function BonzaInit() {
 		}
 	};
 
+	function Applet(xml, context) {
+		var temp;
+		var prop;
+		var local = [];
+		temp = xml.getElementsByTagName("state");
+		if (temp.length != 1) {
+			throw "Fail";
+		}
+		var statename = temp[0].getAttribute("name");
+		temp = xml.getElementsByTagName("init");
+		if (temp.length != 1) {
+			throw "Fail";
+		}
+		var idname = temp[0].getAttribute("id");
+		var initState = firstExpr(temp[0].getElementsByTagName("state")[0]);
+		var initActions = firstExpr(temp[0].getElementsByTagName("actions")[0]);
+		temp = xml.getElementsByTagName("content");
+		if (temp.length != 1) {
+			throw "Fail";
+		}
+		var content = firstExpr(temp[0]);
+		temp = xml.getElementsByTagName("respond");
+		if (temp.length != 1) {
+			throw "Fail";
+		}
+		var inputname = temp[0].getElementsByTagName("input")[0].getAttribute("name");
+		var respState = firstExpr(temp[0].getElementsByTagName("state")[0]);
+		var respActions = firstExpr(temp[0].getElementsByTagName("actions")[0]);
+		var events = [];
+		var instances = [];
+		for (prop in context) {
+			local[prop] = context[prop];
+		}
+		var redraw = function(id, instance) {
+			local[statename] = instance;
+			var element = document.getElementById(id);
+			if (evalExpr(content, local, output)) {
+				element.innerHTML = output.result;
+			}
+		};
+		this.create = function(id) {
+			local[idname] = id;
+			if (evalExpr(initState, local, output)) {
+				instances[id] = output.result;
+				redraw(id, output.result);
+			}
+		};
+		this.broadcast = function(msg) {
+			var instance;
+			for (var id in instances) {
+				instance = instances[id];
+				local[statename] = instance;
+				local[inputname] = msg;
+				if (evalExpr(respState, local, output)) {
+					instances[id] = output.result;
+					redraw(id, output.result);
+				}
+			}
+		};
+		this.destroy = function(id) {
+			instances[id] = undefined;
+		};
+	}
+
+	function Library(xml) {
+		var applets = [];
+		var temp;
+		var name;
+		var context = {
+			core : core
+		};
+		var output = [];
+		var prop;
+		var actions = {
+			redraw : function() {
+				return function(name, id) {
+					applets[name].redraw(id);
+				};
+			},
+			send : function(target, msg) {
+				return function(name, id) {
+					applets[target].broadcast(msg);
+				};
+			}
+		};
+
+		function firstExpr(node) {
+			var first = 0;
+			while (node.childNodes[first].nodeType == 3 && node.childNodes[first].nodeValue.trim() == "") {
+				first++;
+			}
+			return node.childNodes[first];
+		}
+
+		function evalExpr(expr, context, output) {
+			var stmt;
+			var where;
+			var i;
+			var result = { };
+			var context2 = { };
+			var output2 = { };
+			var prop;
+			var array = [];
+			var parser;
+			var xmlDoc;
+			var temp;
+			try {
+				if (expr.nodeType == 3) {
+					return evalFormula(expr.nodeValue.trim(), context, output);
+				}
+				switch(expr.nodeName) {
+				case "text":
+					output.result = expr.innerHTML;
+					break;
+				case "eval":
+					if (evalExpr(firstExpr(expr), context, result)) {
+						if (window.DOMParser) {
+							parser = new DOMParser();
+							xmlDoc = parser.parseFromString(result.result, "text/xml");
+						} else {
+							xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+							xmlDoc.async = false;
+							xmlDoc.loadXML(result.result);
+						}
+						return evalExpr(xmlDoc.children[0], context, output);
+					} else {
+						output.result = undefined;
+						return false;
+					}
+					break;
+				case "list":
+					temp = expr.getElementsByTagName("item");
+					array.length = temp.length;
+					for ( i = 0; i < array.length; i++) {
+						if (evalExpr(temp[i].childNodes[0], context, result)) {
+							array[i] = result.result;
+						} else {
+							output.result = undefined;
+							return false;
+						}
+					}
+					output.result = array;
+					break;
+				case "calc":
+					where = expr.getElementsByTagName("where");
+					for (prop in context) {
+						context2[prop] = context[prop];
+					}
+					for ( i = where.length - 1; i >= 0; i--) {
+						stmt = where[i].children[0];
+						if (evalStmt(stmt, context2, result)) {
+							for (prop in result) {
+								context2[prop] = result[prop];
+							}
+						} else {
+							output.result = undefined;
+							return false;
+						}
+					}
+					return evalExpr(firstExpr(expr), context2, output);
+				case "wrap":
+					for (prop in context) {
+						context2[prop] = context[prop];
+					}
+					for ( i = 0; i < stmt.childNodes.length; i++) {
+						if (stmt.childNodes[i].nodeType != 3) {
+							if (evalStmt(stmt.childNodes[i], context2, result)) {
+								for (prop in result) {
+									context2[prop] = result[prop];
+									output2[prop] = result[prop];
+								}
+								result = { };
+							} else {
+								return false;
+							}
+						}
+					}
+					output.result = output2;
+					break;
+				default:
+					return false;
+				}
+				return true;
+			} catch(error) {
+				return false;
+			}
+		};
+
+		function evalStmt(stmt, context, output) {
+			var name;
+			var i;
+			var prop;
+			var context2 = { };
+			var stmt2;
+			var result = { };
+			try {
+				switch(stmt.nodeName) {
+				case "is":
+					where = expr.getElementsByTagName("where");
+					for (prop in context) {
+						context2[prop] = context[prop];
+					}
+					for ( i = where.length - 1; i >= 0; i--) {
+						stmt2 = where[i].children[0];
+						if (evalStmt(stmt2, context2, result)) {
+							for (prop in result) {
+								context2[prop] = result[prop];
+							}
+						} else {
+							return false;
+						}
+					}
+					return evalExpr(firstExpr(stmt), context2, result);
+				case "not":
+					return !evalStmt(stmt.children[0], context, result);
+				case "def":
+					name = stmt.getAttribute("var");
+					if (evalExpr(firstExpr(stmt), context, result)) {
+						output[name] = result.result;
+					} else {
+						return false;
+					}
+					break;
+				case "all":
+					for (prop in context) {
+						context2[prop] = context[prop];
+					}
+					for ( i = 0; i < stmt.children.length; i++) {
+						if (evalStmt(stmt.children[i], context2, result)) {
+							for (prop in result) {
+								context2[prop] = result[prop];
+								output[prop] = result[prop];
+							}
+							result = { };
+						} else {
+							output = { };
+							return false;
+						}
+					}
+					break;
+				case "any":
+					for ( i = 0; i < stmt.children.length; i++) {
+						if (evalStmt(stmt.children[i], context, result)) {
+							for (prop in result) {
+								output[prop] = result[prop];
+							}
+							return true;
+						}
+					}
+					return false;
+				case "unwrap":
+					if (evalExpr(firstExpr(stmt), context, result)) {
+						for (prop in result.result) {
+							output[prop] = result.result[prop];
+						}
+					}
+					break;
+				default:
+					return false;
+				}
+				return true;
+			} catch(error) {
+				return false;
+			}
+		};
+		temp = xml.getElementsByTagName("common");
+		if (temp.length > 0) {
+			if (temp.length > 1 || !evalStmt(temp[0], context, output)) {
+				throw "Fail";
+			}
+			for (prop in output) {
+				context[prop] = output[prop];
+			}
+		}
+
+		temp = xml.getElementsByTagName("applet");
+		applets.length = temp.length;
+		for (var i = 0; i < temp.length; i++) {
+			name = temp[i].getAttribute("name");
+			applets[name] = new Applet(temp[i]);
+		}
+		//this.applets = applets;
+		var perform = function(actions) {
+			var action;
+			for (var i = 0; i < actions.length; i++) {
+				action = action[i];
+				action();
+			}
+		};
+		var process = function(applet, instance) {
+			var input;
+			var response;
+			var needRedraw = false;
+			for (var i = 0; i < instance.input.length; i++) {
+				input = instance.input[i];
+				response = applet.respond(input, context);
+				instance.state = response.state;
+				instance.actions = response.actions;
+				needRedraw = true;
+			}
+			instance.input = [];
+			return needRedraw;
+		};
+		var run = function() {
+			var applet;
+			var elements;
+			var element;
+			var instance;
+			var id;
+			var name;
+			var content;
+			var setEvents = function(applet, instance, id) {
+				var local;
+				var e;
+				var events = {
+					mousedown : function() {
+						for (prop in output) {
+							local[prop] = context[prop];
+						}
+						local[applet.statename] = instance.state;
+						if (evalExpr(applet.events.mousedown, local, output)) {
+							instance.input.unshift(output.result);
+						}
+						run();
+					}
+				};
+				for (e in applet.events) {
+					jQuery("#" + id).on(e, events[e]);
+				}
+			};
+			var count;
+			do {
+				count = 0;
+				for (name in applets) {
+					applet = applets[name];
+					elements = document.getElementsByClassName("bonza-" + name);
+					for (element in elements) {
+						id = element.getAttribute("id");
+						if (id !== null && id !== "") {
+							instance = applet.instances[id];
+							if ( typeof instance == "undefined") {
+								count++;
+								instance = applet.init(id, context);
+								applet.instances[id] = instance;
+								content = applet.content(instance.state, context);
+								element.innerHTML = content;
+							}
+						}
+					}
+				}
+			} while(count > 0);
+			for (name in applets) {
+				applet = applets[name];
+				for (id in applet.instances) {
+					element = document.getElementById(id);
+					if (element === null) {
+						applet.instances[id] = undefined;
+					} else {
+						instance = applet.instances[id];
+						if (process(applet, instance)) {
+							content = applet.content(instance.state, context);
+							element.innerHTML = content;
+						}
+						perform(instance.actions);
+						instance.actions = [];
+						setEvents(applet, instance, id);
+					}
+				}
+			}
+		};
+		this.run = run;
+	};
+
 	return {
 		evalXMLFile : function(fileName, id) {
 			$("#" + id).html("<span>&nbsp;Loading...&nbsp;</span>");
@@ -536,16 +719,7 @@ function BonzaInit() {
 					$("#" + id).html("<span class='bonza-error'>&nbsp;Status:&nbsp;" + status + "&nbsp;</span>");
 				}
 			}).fail(function(jqXHR, textStatus, errorThrown) {
-				$("#" + id).replaceWith("<span class='bonza-error'>&nbsp;Error:&nbsp;" + textStatus + "&nbsp;</span>");
-			});
-		},
-		runApplet : function(lib, appname, id) {
-			var applet;
-			$.get(lib, function(data, status) {
-				if (status === "success") {
-				} else {
-					$("#" + id).replaceWith("<span class='bonza-error'>&nbsp;Status:&nbsp;" + status + "&nbsp;</span>");
-				}
+				$("#" + id).html("<span class='bonza-error'>&nbsp;Error:&nbsp;" + textStatus + "&nbsp;</span>");
 			});
 		}
 	};
